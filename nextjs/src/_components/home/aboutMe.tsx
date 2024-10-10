c00827ff428b5a080c97b225acde8e3264ef01f6
gabgab <gabifiil68@gmail.com>
2024-10-10 09:41:18 +0200

    Comment

--- nextjs/src/_components/home/aboutMe.tsx
+++ nextjs/src/_components/home/aboutMe.tsx
@@ -4,14 +4,15 @@ import Marquee from "react-fast-marquee";
 export default function AboutMe() {
     return (
         <section id={"about"} className="flex flex-col items-center gap-5">
-            <Marquee speed={200} className="h-28 bg-whiteAlpha-100 uppercase">
+            {/*<Marquee speed={200} className="h-28 bg-whiteAlpha-100 uppercase">
                 <h1 className="h1 mr-12">full stack</h1>
                 <h1 className="h1 mr-12">Php</h1>
                 <h1 className="h1 mr-12">react</h1>
                 <h1 className="h1 mr-12">JS</h1>
                 <h1 className="h1 mr-12">POSTGRES</h1>
                 <h1 className="h1">NEXT</h1>
-            </Marquee>
+            </Marquee>*/}
+
             <div className="flex flex-col items-center justify-center gap-3 py-6 s-pho:px-4 l-pho:px-4">
                 <h2 className="h2">À propos de moi</h2>
                 <div className="flex flex-col items-center justify-center text-center">
@@ -19,14 +20,14 @@ export default function AboutMe() {
                     <p className="l16-text-reg">Je suis actuellement alternance chez Ozzak et en dernière année d&apos;étude pour le...</p>
                 </div>
             </div>
-            <Marquee direction={"right"} speed={200} className="flex h-36 gap-2 bg-whiteAlpha-100 uppercase">
+            {/*<Marquee direction={"right"} speed={200} className="flex h-36 gap-2 bg-whiteAlpha-100 uppercase">
                 <h1 className="h1 mr-12">full stack</h1>
                 <h1 className="h1 mr-12">Php</h1>
                 <h1 className="h1 mr-12">react</h1>
                 <h1 className="h1 mr-12">JS</h1>
                 <h1 className="h1 mr-12">POSTGRES</h1>
                 <h1 className="h1">NEXT</h1>
-            </Marquee>
+            </Marquee>*/}
         </section>
     );
 }
