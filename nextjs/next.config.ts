e809bb13a20572d72a14b0c5bbf1ad49001b19e9
gabgab <gabifiil68@gmail.com>
2025-02-10 13:11:28 +0100

    Update next.js and strapi

---
+++ nextjs/next.config.ts
@@ -0,0 +1,59 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import {NextConfig} from "next";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+    reactStrictMode: process.env.NEXT_ENV === 'dev',
+    env: {
+        STRAPI_URL: process.env.NEXT_PUBLIC_STRAPI_URL,
+        STRAPI_TOKEN: process.env.NEXT_PUBLIC_STRAPI_TOKEN,
+        NEXT_ENV: process.env.NEXT_ENV,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    //output: 'export',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: false,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'strapi.gabriaile.dev',
+                port: '',
+            },
+        ],
+    },
+    compress: true,
+    bundlePagesRouterDependencies: true,
+    devIndicators: {
+        appIsrStatus: true,
+        buildActivity: true,
+        buildActivityPosition: 'bottom-right',
+    },
+    experimental: {
+        staleTimes: {
+            dynamic: 30,
+        },
+        turbo: {
+            resolveAlias: {
+                'next-intl/config': './path/to/i18n.ts',
+            },
+        }
+    },
+};
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(nextConfig);
