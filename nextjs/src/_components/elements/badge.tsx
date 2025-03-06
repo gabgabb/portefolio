2c613fabb6ae91bc36c205d95c33ba64e6216a6b
gabgab <gabifiil68@gmail.com>
2025-03-07 00:58:42 +0100

    Create badge for technos

---
+++ nextjs/src/_components/elements/badge.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import {Chip} from "@heroui/chip";
+import {Image} from "@heroui/react";
+
+interface BadgeProps {
+    key?: number;
+    image?: string;
+    title?: string;
+}
+
+const Badge: React.FC<BadgeProps> = ({
+    image,
+    title
+}) => {
+    return (
+        <Chip className="flex bg-blue-gray h-max rounded-md p-2" startContent={<Image width={25} height={25} src={image}/>} variant="faded">
+            <span className="font-semibold">
+                {title}
+            </span>
+        </Chip>
+    );
+}
+
+export default Badge;
