654a60a89af99fce4f6290ac803d4681df55fea3
gabgab <gabifiil68@gmail.com>
2025-03-07 01:01:21 +0100

    Change base layout

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,21 +1,26 @@
+import React from "react";
 import AboutMe from "@/_components/home/aboutMe";
 import Projects from "@/_components/home/projects";
-import React from "react";
 import {Experiences} from "@/_components/home/experiences";
 import {Technologies} from "@/_components/home/technologies";
 
 const Home: React.FC = () => {
     return (
-        <>
-            <div
-                className="bg-gradient-to-b from-[#27325a] to-transparent absolute block box-border left-0 right-0 top-0 h-[480px] opacity-60 z-[-1]"></div>
+        <div className="flex flex-col justify-center items-center mx-auto w-full max-w-[1000px]">
+            {/* Dégradé en fond */}
+            <div className="bg-gradient-to-b from-[#27325a] to-transparent absolute left-0 right-0 top-0 h-[480px] opacity-60 z-[-1]"></div>
+
+            {/* Section À Propos */}
             <AboutMe/>
-            <div className="flex justify-center gap-24">
+            {/* Section Technologies & Expériences */}
+            <div className="flex gap-4">
                 <Technologies/>
-                <Experiences/>
+                <Experiences />
             </div>
+
+            {/* Projets */}
             <Projects/>
-        </>
+        </div>
     );
 }
 
