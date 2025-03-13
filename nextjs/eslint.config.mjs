752cd183130b5c573b7546f1cf879320b9b703c1
gabgab <gabifiil68@gmail.com>
2025-03-13 17:07:36 +0100

    Prettier

--- nextjs/eslint.config.mjs
+++ nextjs/eslint.config.mjs
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import prettier from "eslint-plugin-prettier";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
